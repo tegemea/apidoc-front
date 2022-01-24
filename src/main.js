767176ead81bd8a9ade0e99fd13a8ce4a53887f8
lema <lemajunior@hotmail.com>
2022-01-24 16:43:41 +0300

    installed & configured bootstrap-(vue), jquery, & vue-router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Axios from 'axios'
+import VueRouter from 'vue-router'
+import jQuery from 'jquery'
+import 'bootstrap'
 
 Vue.config.productionTip = false
 
 Vue.prototype.$axios = Axios;
+Vue.prototype.$jquery = jQuery;
+
+Vue.use(VueRouter)
 
 new Vue({
   render: h => h(App),
