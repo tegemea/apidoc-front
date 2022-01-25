4acee2db0f095b374ec9e37ac5da7030d5c0568e
lema <lemajunior@hotmail.com>
2022-01-25 14:58:17 +0300

    sass preprocessor installed & working globally

--- src/main.js
+++ src/main.js
@@ -3,15 +3,36 @@ import App from './App.vue'
 import Axios from 'axios'
 import VueRouter from 'vue-router'
 import jQuery from 'jquery'
-import 'bootstrap'
+import 'bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.config.productionTip = false
 
 Vue.prototype.$axios = Axios;
 Vue.prototype.$jquery = jQuery;
 
+// IPs for communications
+Vue.prototype.$baseURL = '192.168.1.2';
+Vue.prototype.$apiURL = '192.168.1.2/api_doc';
+
 Vue.use(VueRouter)
 
+import Home from '@/components/Home'
+import Documentation from '@/components/Documentation'
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/doc', component: Documentation }
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
